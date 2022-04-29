bdbbb5eb41cf91936ae4cec63ebf3cdb0d2c6c09
rahidhaque <ontirahid@yahoo.com>
2022-04-29 17:56:41 +0600

    Navbar Desktop Done

---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faSeedling, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!click);
+    }
+    return (
+        <div>
+            <nav className='nav-items'>
+                <h3 className='nav-logo'>Super Grocery Factory  <i><FontAwesomeIcon className='fa-logo' icon={faSeedling} /></i></h3>
+                <div className='menu-icon' onClick={handleClick}>
+                    {click ? <FontAwesomeIcon className='fa-logo' icon={faTimes} /> : <FontAwesomeIcon className='fa-logo' icon={faBars} />}
+                </div>
+                <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/manageitems">Manage Items</Link></li>
+                    <li><Link to="/additems">Add Items</Link></li>
+                    <li><Link to="/myitems">My Items</Link></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
