ab78929a5cb5e44a2814c79ab497223ada1a0187
rahidhaque <ontirahid@yahoo.com>
2022-04-29 19:17:52 +0600

    Banner Added with freepik

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
