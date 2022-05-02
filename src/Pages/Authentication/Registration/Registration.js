4b36c4452095df56955ef413cbe9e71e50e4176e
rahidhaque <ontirahid@yahoo.com>
2022-05-02 10:59:44 +0600

    Register User Done

--- src/Pages/Authentication/Registration/Registration.js
+++ src/Pages/Authentication/Registration/Registration.js
@@ -1,42 +1,80 @@
 import { Icon } from '@iconify/react';
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import registration from '../../../images/Authentication/Registration.png';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import Loading from '../../Shared/Loading/Loading';
 
 const Registration = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passRef = useRef();
+    const navigate = useNavigate();
+    let errorText;
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleRegistration = async event => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const pass = passRef.current.value;
+
+        createUserWithEmailAndPassword(email, pass);
+    }
+
+    if (user) {
+        navigate('/');
+    }
+
+    if (loading) {
+        return <Loading></Loading>;
+    }
+
+    if (error) {
+        errorText = <p className='text-danger'>Error: {error?.message}</p>
+    }
     return (
         <div>
             <div className='d-flex justify-content-center align-items-center mt-5'>
                 <div>
-                    <img className='w-75 h-50' src={registration} alt="" />
+                    <img className='w-75' src={registration} alt="" />
                 </div>
                 <div className='w-25 form-login mr-5'>
-                    <Form>
+                    <Form onSubmit={handleRegistration} autoComplete="off">
                         <h3 className='text-center'><Icon icon="carbon:user-role" /></h3>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Name" required />
+                            <Form.Control ref={nameRef} type="text" placeholder="Enter Name" required />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required />
+                            <Form.Control ref={emailRef} type="email" placeholder="Enter email" autoComplete='off' required />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required />
+                            <Form.Control ref={passRef} type="password" placeholder="Password" autoComplete='off' required />
                         </Form.Group>
                         <Button className='w-100' variant="danger" type="submit">
                             Register <span> <Icon icon="eos-icons:system-re-registered" /></span>
                         </Button>
+                        <div className='mt-2 fw-bold'>
+                            {errorText}
+                        </div>
                         <p className='mt-2 text-center'>
                             Already have an account? <Link className='text-decoration-none text-danger' to="/login">Login</Link>
                         </p>
                     </Form>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
