feb75ae64f6fe134b2992f1759e488a2fedd8a6f
rahidhaque <ontirahid@yahoo.com>
2022-05-08 16:27:26 +0600

    Home Page Loading Fix from database

--- src/Pages/Home/Inventories/Inventories.js
+++ src/Pages/Home/Inventories/Inventories.js
@@ -1,16 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import useInventories from '../../../hooks/useInventories';
+import Loading from '../../Shared/Loading/Loading';
 import Inventory from '../Inventory/Inventory';
 import './inventories.css'
 
 const Inventories = () => {
-    const [inventories] = useInventories();
+    const [loading, setLoading] = useState(true);
+    const [inventories, setInventories] = useState([]);
+
+    useEffect(() => {
+        const fetchInventory = async () => {
+            setLoading(true);
+            await fetch('https://arcane-brook-72001.herokuapp.com/inventory')
+                .then(res => res.json())
+                .then(data => setInventories(data));
+            setLoading(false);
+        }
+        fetchInventory();
+    }, [])
+
+
 
     return (
         <div className='container'>
-            <div className='row'>
-                <h2 className='text-center my-5'>Our Dealing Inventories</h2>
+            <h2 className='text-center my-5'>Our Dealing Inventories</h2>
+            {loading ? <Loading></Loading> : <div className='row'>
                 <div className='inventories-container my-2'>
                     {
                         inventories.slice(0, 6).map(inventory => <Inventory
@@ -25,6 +40,8 @@ const Inventories = () => {
                     </Link>
                 </div>
             </div>
+            }
+
         </div>
 
     );
--- src/hooks/useInventories.js
+++ src/hooks/useInventories.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import Loading from "../Pages/Shared/Loading/Loading";
 
 const useInventories = () => {
     const [inventories, setInventories] = useState([]);
     useEffect(() => {
+
         fetch('https://arcane-brook-72001.herokuapp.com/inventory')
             .then(res => res.json())
             .then(data => setInventories(data))
+
     }, [])
     return [inventories, setInventories];
 }
